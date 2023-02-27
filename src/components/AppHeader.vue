5ff465e484fff79ebefd0242fe8dd23504def85c
MichelepioBombai <michelebombai@icloud.com>
2023-02-27 16:57:11 +0100

    add header/footer

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from "vue";
+</script>
+
+<template>
+  <header>
+    <div class="bg-black"></div>
+    <div>
+      <div class="container">
+        <img src="../assets/dc-logo.png" alt="" />
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.bg-black {
+  background-color: black;
+  height: 100px;
+}
+</style>
